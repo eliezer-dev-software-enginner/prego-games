b82adbd7bbb2251630cc8def06d97f7f7d64fa51
Eliezer Assunção De Paulo <93846923+eliezer-dev-software-enginner@users.noreply.github.com>
2026-04-30 10:27:37 -0300

    Update route.ts

--- app/api/roms/[id]/route.ts
+++ app/api/roms/[id]/route.ts
@@ -1,6 +1,7 @@
-import { NextResponse } from 'next/server';
-import { verifyAdmin } from '../../../admin/_utils/utils';
-import { adminDb } from '../../../config/firebase-admin';
+// app/api/roms/[id]/route.ts
+import { NextResponse } from "next/server";
+import { verifyAdmin } from "../../../admin/_utils/utils";
+import { adminDb } from "../../../config/firebase-admin";
 
 export async function PUT(
   req: Request,
@@ -8,12 +9,12 @@ export async function PUT(
 ) {
   const admin = await verifyAdmin();
   if (!admin)
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
   const { id } = await params;
   const body = await req.json();
 
-  await adminDb.collection('apps/prego-games/roms').doc(id).update(body);
+  await adminDb.collection("apps/prego-games/roms").doc(id).update(body);
 
   return NextResponse.json({ id });
 }
@@ -24,11 +25,34 @@ export async function DELETE(
 ) {
   const admin = await verifyAdmin();
   if (!admin)
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
   const { id } = await params;
 
-  await adminDb.collection('apps/prego-games/roms').doc(id).delete();
+  await adminDb.collection("apps/prego-games/roms").doc(id).delete();
 
   return NextResponse.json({ id });
+}
+
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+
+  const doc = await adminDb.collection("apps/prego-games/roms").doc(id).get();
+
+  if (!doc.exists) {
+    return NextResponse.json({ error: "Rom not found" }, { status: 404 });
+  }
+
+  const data = doc.data();
+
+  // remove campos sensíveis
+  const { pathRef, ...rest } = data as any;
+
+  return NextResponse.json({
+    id: doc.id,
+    ...rest,
+  });
 }
